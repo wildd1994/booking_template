06fc7e630dfd08633d1cc0fdc5e11b81b585c41f
suvorov <dfffgfgf@gmail.com>
2023-08-07 14:32:20 +0300

    init skeleton

---
+++ booking_finam/booking_finam/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for booking_finam project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_finam.settings')
+
+application = get_asgi_application()
