c058aec9df15449ac0f348c46b0a67b6de593715
suvorov <dfffgfgf@gmail.com>
2023-08-08 11:50:51 +0300

    redirect to user app

--- booking_finam/booking_finam/urls.py
+++ booking_finam/booking_finam/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('/', include('booking.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('registration/', include('django.contrib.auth.urls')),
+    path('registration/', include('users.urls'))
+
 ]
