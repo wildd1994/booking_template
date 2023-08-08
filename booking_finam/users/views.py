38b92c043f3421eb9f1e03a2c45ec946335c9221
suvorov <dfffgfgf@gmail.com>
2023-08-08 11:50:38 +0300

    add custom signup form

--- booking_finam/users/views.py
+++ booking_finam/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import MyCreationForm
+from .models import User
+
+
+class SignUpView(CreateView):
+    model = User
+    form_class = MyCreationForm
+    success_url = '/registration/login'
+    template_name = 'registration/signup.html'
