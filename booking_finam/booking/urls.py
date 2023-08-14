fea7b51569a1508b5dcb499e25feacc4b9983130
suvorov <dfffgfgf@gmail.com>
2023-08-14 11:04:26 +0300

    add creaate view

--- booking_finam/booking/urls.py
+++ booking_finam/booking/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-# Импортируем созданное нами представление
+
 from .views import (
    BookingDetail,
    BookingDelete,
    booking_update,
    my_bookings,
    my_schedule,
-   validate_time_slots
+   validate_time_slots,
+   create_booking
 )
 
 
@@ -16,6 +17,6 @@ urlpatterns = [
    path('<int:pk>', BookingDetail.as_view(), name='booking_detail'),
    path('<int:pk>/update/', booking_update, name='booking_update'),
    path('<int:pk>/delete/', BookingDelete.as_view(), name='booking_delete'),
-   path('validate_timeslots', validate_time_slots, name='validate_username')
-   # path('create/', booking, name='choice_booking')
+   path('validate_timeslots', validate_time_slots, name='validate_username'),
+   path('create/', create_booking, name='create_booking')
 ]
